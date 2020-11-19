ff8d87c347789bef3760cc785edfb9aec8625c86
mobster <74624559+mobieto@users.noreply.github.com>
2020-11-19 13:07:42 +0000

    Update main.py

--- test bot/main.py
+++ test bot/main.py
@@ -1,24 +1,96 @@
-import discord
+import discord, datetime
 from discord.ext import commands
-import botcmds
+from Modules import bot_cmds, bot_util
 
 TOKEN = ''
 PREFIX = '-'
 
-client = discord.Client()
+ALIASES = {
+        'ppsize': ['peepeesize'],
+        'serverlock': ['slock'],
+        'serverunlock': ['sunlock'],
+        'fact': ['randomfact'],
+        'lovecalculator': ['lcalculator', 'lc']
+    }
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity = discord.Game(name = "-help"))
     print('Bot online')
 
 @client.event
+async def on_member_join(user):
+    await user.send('Welcome to '+user.guild.name)
+    await user.add_roles(user.guild.get_role(778695313677484089))
+
+    content = f'**Username:** {user.mention} ({user.id})\n'
+    content += f'**Time:** {datetime.datetime.utcnow().strftime("%d %b %Y, %H:%M:%S UTC")}'
+    
+    log_channel = client.get_channel(778365005056835624)
+    Embed = await bot_util.get_embed('User Joined', content)
+    await log_channel.send(embed=Embed)
+
+@client.event
+async def on_member_remove(user):
+    content = f'**Username:** {user.name} ({user.id})\n'
+    content += f'**Time:** {datetime.datetime.utcnow().strftime("%d %b %Y, %H:%M:%S UTC")}'
+    
+    log_channel = client.get_channel(778365005056835624)
+    Embed = await bot_util.get_embed('User Left', content)
+    await log_channel.send(embed=Embed)
+
+@client.event
+async def on_message_delete(msg):
+    content = f'**User:** {msg.author.mention} ({msg.author.id})\n'
+    content += f'**Channel:** {msg.channel.mention} ({msg.channel.id})\n'
+    content += f'**Time:** {datetime.datetime.utcnow().strftime("%d %b %Y, %H:%M:%S UTC")}\n'
+    content += f'**Message:** {msg.content}'
+
+    log_channel = client.get_channel(778365005056835624)
+    Embed = await bot_util.get_embed('Message Deleted', content)
+    await log_channel.send(embed=Embed)
+
+@client.event
+async def on_message_edit(old, new):
+    if old.content != new.content:
+        content = f'**User:** {new.author.mention} ({new.author.id})\n'
+        content += f'**Channel:** {new.channel.mention} ({new.channel.id})\n'
+        content += f'**Time:** {datetime.datetime.utcnow().strftime("%d %b %Y, %H:%M:%S UTC")}\n'
+        content += f'**Old Message:** {old.content}\n'
+        content += f'**New Message:** {new.content}'
+
+        log_channel = client.get_channel(778365005056835624)
+        Embed = await bot_util.get_embed('Message Edited', content)
+        await log_channel.send(embed=Embed)
+
+@client.event
 async def on_message(msg):
-    if msg.content[0] == PREFIX:
-        cmd = msg.content[1:len(msg.content)]
+    if msg.content.startswith(PREFIX):
+        args = msg.content.split(' ')
+        cmd = args[0][1:len(args[0])].lower()
+        del args[0]
         
-        if hasattr(botcmds, cmd):
-            result = await getattr(botcmds, cmd)(msg)
+        if hasattr(bot_cmds, cmd):
+            result = await getattr(bot_cmds, cmd)(msg, args)
+        else:
+            alias = ''
+            for name in ALIASES.keys():
+                if cmd in ALIASES[name]:
+                    alias = name
+                    break
+
+            if alias != ''  and hasattr(bot_cmds, alias):
+                result = await getattr(bot_cmds, alias)(msg, args)
+            else:
+                await msg.add_reaction('❓')
+    else:
+        if msg.channel == client.get_channel(778616463530786826):
+            await msg.add_reaction('👍')
+            await msg.add_reaction('👎')
 
 client.run(TOKEN)
-
-
