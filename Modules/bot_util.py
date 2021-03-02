457a2e2c5db028d28b35455724500521a376bd6c
unknown <hmobin2004@gmail.com>
2021-03-02 15:35:05 +0000

    ete

--- Modules/bot_util.py
+++ Modules/bot_util.py
@@ -30,7 +30,7 @@ async def get_image_file_from_url(url):
     try:
         async with session.get(url) as response:
             stream = io.BytesIO(await response.read())
-            return discord.File(stream, filename='image.png')
+            return open(stream)
     except Exception as e:
         return e
 
