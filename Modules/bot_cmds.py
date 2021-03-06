0897987131ed300897a30d28ff13be81c2dd4aa8
unknown <hmobin2004@gmail.com>
2021-03-06 16:54:20 +0000

    ye

--- Modules/bot_cmds.py
+++ Modules/bot_cmds.py
@@ -93,7 +93,7 @@ async def wikipedia(msg, args, client=None):
     page = WIKI_API.get_page(arg)
     try:
         await msg.channel.send(await page.summary())
-        await msg.channel.send('<'+await page.urls().view+'>')
+        await msg.channel.send('<'+await page.urls()[0]+'>')
     except Exception as e:
         await msg.channel.send(e)
 
