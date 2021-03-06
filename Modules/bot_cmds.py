64f45376944e25ae4f770c75492836d2eaace04e
unknown <hmobin2004@gmail.com>
2021-03-06 17:12:11 +0000

    yes

--- Modules/bot_cmds.py
+++ Modules/bot_cmds.py
@@ -1,4 +1,4 @@
-import os, io, discord, json, random, aiohttp, aiowiki
+import os, io, discord, json, random, aiohttp, aiowiki, time
 from Modules import bot_util
 from PIL import ImageFilter
 
@@ -63,6 +63,8 @@ session = bot_util.session
 
 # GENERAL PURPOSE
 
+rate_limits = {}
+
 async def ping(msg, args, client=None):
     await msg.channel.send('Pong!')
 
@@ -89,13 +91,22 @@ async def cat(msg, args, client=None):
         await msg.channel.send(embed=embed)
 
 async def wikipedia(msg, args, client=None):
-    arg = ' '.join(args)
-    page = WIKI_API.get_page(arg)
-    try:
-        await msg.channel.send(await page.summary())
-        await msg.channel.send('<'+str(await page.urls()).split("'")[1]+'>')
-    except Exception as e:
-        await msg.channel.send(e)
+    if not msg.author.name in rate_limits:
+        rate_limits[msg.author.name] = True
+
+
+        arg = ' '.join(args)
+        page = WIKI_API.get_page(arg)
+        try:
+            await msg.channel.send(await page.summary())
+            await msg.channel.send('<'+str(await page.urls()).split("'")[1]+'>')
+        except Exception as e:
+            await msg.channel.send(e)
+
+        time.sleep(2)
+        rate_limits.pop(msg.author.name, None)
+    else:
+        msg.channel.send('SLOW DOWN THERE BUDDY')
 
 async def avatar(msg, args, client=None):
     if len(msg.mentions) > 0:
